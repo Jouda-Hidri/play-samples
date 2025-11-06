e4cd46d1beead19aafe8bbbc849a67f2f9847d81
Jouda Hidri <jouda.hidri@token.io>
2025-11-06 13:20:58 +0100

    Add to sorted map

--- play-scala-hello-world-tutorial/app/controllers/SearchController.scala
+++ play-scala-hello-world-tutorial/app/controllers/SearchController.scala
@@ -2,19 +2,34 @@ package controllers
 
 import javax.inject._
 import play.api.mvc._
+import scala.collection.mutable
+
 
 @Singleton
 class SearchController @Inject()(val controllerComponents: ControllerComponents)
     extends BaseController {
 
-def showForm = Action { implicit request: Request[AnyContent] =>
-  Ok(views.html.searchForm(request))
-}
+  def showForm = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.searchForm(request))
+  }
+
+  val words = mutable.Map[String, Int]()
 
 
   def submitSearch = Action { implicit request =>
     val searchTerm = request.body.asFormUrlEncoded
       .flatMap(_.get("query").flatMap(_.headOption))
-    Ok(s"You searched for: ${searchTerm.getOrElse("")}")
+
+    searchTerm match {
+      case Some(word) =>
+        // ✅ word is now a String
+        words.update(word, words.getOrElse(word, 0) + 1)
+        val sorted = words.toSeq.sortBy(-_._2)
+        val top5 = sorted.take(5)
+        Ok(s"Updated word map: $top5")
+
+      case None =>
+        BadRequest("No query provided")
+    }
   }
 }
